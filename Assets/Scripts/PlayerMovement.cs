d627dbc9600e007b429596db41874e03892a65b5
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2023-03-04 12:48:40 -0500

    Refactor variable height jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,21 +11,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpMagnitude = 200f;
     [SerializeField] private float movementMagnitude = 5f;
 
-    // Variable jump related variables
-    [SerializeField] private float _maxJumpTime = 0.5f;
-    [SerializeField] private float _jumpAcceleration = 5f;
+    // Variable jump height related variables
+    [SerializeField] private float maxJumpTime = 0.5f;
+    [SerializeField] private float jumpAcceleration = 5f;
 
-    private Rigidbody2D _rigidbody;
-    private PlayerInput _playerInput;
     private PlayerInputActions _inputActions;
-    // Variable jump related variables
     private bool _isJumping = false;
-    private bool _pressingJump = false;
     private float _startJumpTime;
     private void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _playerInput = GetComponent<PlayerInput>();
         _inputActions = new PlayerInputActions();
         _inputActions.Enable();
     }
@@ -38,11 +33,8 @@ public class PlayerMovement : MonoBehaviour
     private void CheckMovement()
     {
         var direction = _inputActions.Player.Move.ReadValue<Vector2>();
-        if (direction.y < 0)
-        {
-            DropDown();
-        }
-        
+        if (direction.y < 0) DropDown();
+
         // We don't want vertical movement to be handled by 'W' and 'S'
         direction.y = 0;
         playerBody.AddForce(direction * movementMagnitude);
@@ -50,31 +42,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void CheckJump()
     {
-        // If the key is pressing
-        if (_inputActions.Player.Jump.ReadValue<float>() > 0.5)
+        bool jump = _inputActions.Player.Jump.ReadValue<float>() > 0.5;
+        if (!jump) return;
+        
+        // First jump
+        // Each press will only contribute to one first jump.
+        // If the player continues pressing the jump key, when the object hits the ground, the object will not jump continuously.
+        // Players have to release the jump key and press it again to perform another jump.
+        if (!_isJumping)
         {
-            // First jump
-            // Each press will only contribute to one first jump.
-            // If the player continues pressing the jump key, when the object hits the ground, the object will not jump continuously.
-            // Players have to release the jump key and press it again to perform another jump.
-            if (!_pressingJump && !_isJumping)
-            {
-                Debug.Log("Jump");
-                _isJumping = true;
-                _startJumpTime = Time.time;
-                playerBody.AddForce(Vector2.up * jumpMagnitude);
-            }
-            // Variable jump height
-            if (_pressingJump && _isJumping && Time.time - _startJumpTime < _maxJumpTime)
-            {
-                playerBody.AddForce(Vector2.up * _jumpAcceleration);
-            }
-            _pressingJump = true;
+            Debug.Log("Jump");
+            _isJumping = true;
+            _startJumpTime = Time.time;
+            playerBody.AddForce(Vector2.up * jumpMagnitude);
         }
-        else
+        else if (IsBelowMaxJump()) 
+        // Variable jump height
         {
-            _pressingJump = false;
+            playerBody.AddForce(Vector2.up * jumpAcceleration);
         }
+    }
+
+    private bool IsBelowMaxJump()
+    {
+        return Time.time - _startJumpTime < maxJumpTime;
     }
 
 
